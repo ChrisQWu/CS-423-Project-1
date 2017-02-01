f5bc04963e4837ee8a5d4d687f7337b3114d3070
ChrisWuUNM <cwu@unm.edu>
2017-02-01 01:23:25 -0700

    Sensitivity.m -Added a subplot for figure 2.b -This figure takes the difference of the values from the diff eqn  and plots it vs time steps

--- Sensitivity_to_Initial_Conditions.m
+++ Sensitivity_to_Initial_Conditions.m
@@ -20,9 +20,14 @@ y1 = 0.66;
 ys = dynamical(x1,y1,ro,gamma,n);
 f1 = ys;
 
-figure(1)
+figure(2)
+subplot(1,2,1);
 plot(ts,f0,ts,f1)
 xlabel('time steps')
 ylabel('y values')
 legend('x_0 = 0.66, y_0 = 0.11', 'x_0 = 0.66, y_0 = 0.66')
-hold on;
+
+subplot(1,2,2);
+plot(ts,abs(f0-f1))
+xlabel('time steps')
+ylabel('difference in y values')

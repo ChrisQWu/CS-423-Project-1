1548eef3e59e640f8cc138e2cfbcef8e8cc69d74
Alex Johnson <alex_johnson3@yahoo.com>
2017-02-05 12:24:44 -0700

    Simplify dynamical map calculation code and include starting points
    Initialize the results with the initial conditions and perform 't' steps.

--- dynamical.m
+++ dynamical.m
@@ -1,12 +1,9 @@
-function [xs,ys] = dynamical(x0, y0, ro, gamma, t)
-    xs = zeros(t,1);
-    ys = zeros(t,1);
-    x = x0;
-    y = y0;
-    for n = 1:t
-        xs(n) = ro - x^2 + gamma * y;
-        ys(n) = x;
-        x = xs(n);
-        y = ys(n);
+function [x, y] = dynamical(x0, y0, rho, gamma, t)
+    x = [x0; zeros(t,1)];
+    y = [y0; zeros(t,1)];
+
+    for n = 2:t+1
+        x(n) = rho - x(n-1)^2 + gamma * y(n-1);
+        y(n) = x(n-1);
     end
 end

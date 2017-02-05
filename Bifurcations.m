c23d6b0323f7144ae280d3577d1aa4e15d4607a5
ChrisWuUNM <cwu@unm.edu>
2017-02-04 21:45:12 -0700

    Bifurcations.m -Added comments
    Dynamical_Flow.m
    -Added a .m file intended for Part 2 of the project

--- Bifurcations.m
+++ Bifurcations.m
@@ -1,12 +1,15 @@
 n = 1000; %number of steps
 gamma = 0.3;
-ros = linspace(0.5,1.5,n);
+ros = linspace(0.5,1.5,n);%array of all ro's between the given range with
+                           % n steps
 %initial conditions
 x0 = 0.135649;
 y0 = 0.546912;
+
 for i = 1:n
-    xs = dynamical(x0,y0,ros(i),gamma,500);
-    for k =450:500
+    xs = dynamical(x0,y0,ros(i),gamma,500);%get all x values of the 
+                                        %logistic map
+    for k =450:500%skip the transient values and plot the end points
         f = xs(k);
         ro = ros(i);
         plot(ro, f,'.b');
---
+++ Dynamical_Flow.m
@@ -0,0 +1,12 @@
+alpha = 0.2;
+beta = 0.2;
+gamma = 5.7;
+
+% alpha = 0.1;
+% beta = 0.1;
+% gamma = 14;
+
+%x-prime, y-prime, z-prime
+xp = x*y - x*gamma + alpha;
+yp = -z - x;
+zp = beta*z + y;

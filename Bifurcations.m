e2e9c59a9587743254cb423c6038e18ef9df6b7c
ChrisWuUNM <cwu@unm.edu>
2017-02-04 21:37:12 -0700

    Bifurcations.m -Changed the number of values plotted that is associated with the ro from 100 to 50
    Added the plot for part 2.a and 2.b

--- Bifurcations.m
+++ Bifurcations.m
@@ -6,7 +6,7 @@ x0 = 0.135649;
 y0 = 0.546912;
 for i = 1:n
     xs = dynamical(x0,y0,ros(i),gamma,500);
-    for k =400:500
+    for k =450:500
         f = xs(k);
         ro = ros(i);
         plot(ro, f,'.b');

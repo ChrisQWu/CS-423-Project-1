025ed805a6461864053d9174129018632bb3d5de
ChrisWuUNM <cwu@unm.edu>
2017-02-09 18:00:47 -0700

    Bifurcations.m -Does not work -Instead use Bifurcation_of_c.m - code was found at https://www.mathworks.com/matlabcentral/fileexchange/56600-the-rossler-attractor--chaotic-simulation
    Signed-off-by: ChrisWuUNM <cwu@unm.edu>

--- Bifurcations3d.m
+++ Bifurcations3d.m
@@ -1,23 +1,22 @@
 function Bifurcations3d
-    n = 1000; %number of steps                       
-    gammas = linspace(0,30,n);
 %     betas = linspace(2,0.2,n);
 %     alphas = linspace(0.2,0.56,n);
     x0 = 0.5;
     y0 = 0.4;
     z0 = 0.3;
     dt = 0.01;
+    t = 1000;
     alpha = 0.2;
-%     beta = 0.2;
+    beta = 0.2;
 %     gamma = 5.7;
     figure();
-    for i = 1:n
-        [x,y,z] = Dynamical_Flow([x0;y0;z0],alpha,beta,gammas(i),dt,10000);
-        plot(gammas(i), z(9900:10000),'.b','MarkerSize',1)
+    for gamma = 0:0.01:45.0
+        [x,y,z] = Dynamical_Flow([x0;y0;z0],alpha,beta,gamma,dt,t);
+        plot(gamma, z((t-100):t),'.b','MarkerSize',1)
         hold on;
     end
     xlabel('\gamma');
     ylabel('z');
-    title(['Bifurcations w/ x_0 = ' num2str(x0) ', y_0 = ' num2str(y0) ', z_0 = ' num2str(z0) ', n = ' num2str(n) ]);
+    title(['Bifurcations w/ x_0 = ' num2str(x0) ', y_0 = ' num2str(y0) ', z_0 = ' num2str(z0) ', t = ' num2str(t) ]);
 
 end
